47289e01b64220345fe4a7b3f4242f0690c6a2eb
JGP345 <jorgeprias345@gmail.com>
2022-06-06 11:25:20 -0500

    why

--- backend/settings.py
+++ backend/settings.py
@@ -134,6 +134,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
 } 
 django_heroku.settings(locals())
